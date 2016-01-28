bb47840de867c7784f060d52316e81436c54b50e
ricmansousa <ricardomansousa@gmail.com>
2016-01-28 08:33:32 +0000

    Características dos sensores pluviosidade e uv criadas

---
+++ gcc/_build/uv.d
@@ -0,0 +1,7 @@
+_build/uv.o: \
+ /home/ricardo/Documentos/allahuakbar/drivers/sensors/ambient/uv.c \
+ /home/ricardo/Documentos/allahuakbar/drivers/sensors/ambient/uv.h \
+ /home/ricardo/Documentos/allahuakbar/config/ble_services_config/ambient_service_config.h \
+ /home/ricardo/Documentos/allahuakbar/config/board_config.h \
+ /home/ricardo/Documentos/allahuakbar/config/board_config/sensingplus/boron_config.h \
+ /home/ricardo/Documentos/nrf51/tools/gcc-arm-none-eabi-4_9-2015q3/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h
